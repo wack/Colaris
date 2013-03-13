398f4eb423f79e61248933f229645d92c53b4e2c
Laura Wacker <wackerlaura@gmail.com>
2013-03-12 20:43:54 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colaris::Application.config.secret_token = 'bf5b98c85806e6a91a14fd37f7d7d4d4c81bf612b16363629cb206318f14f6ef1d952fef32dcbf63823aace03a5022461e26ca37519bc78ea3106f5ce12c227f'
